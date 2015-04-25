cdb9aacb311c60b355551160208338173177d321
CanDD <canddk44bk@gmail.com>
2015-04-24 19:11:17 -0700

    Add mobile shop

---
+++ upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.116.131/admin/');
+define('HTTP_CATALOG', 'http://192.168.116.131/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.116.131/admin/');
+define('HTTPS_CATALOG', 'http://192.168.116.131/');
+
+// DIR
+define('DIR_APPLICATION', '/home/candd/www/opencart/upload/admin/');
+define('DIR_SYSTEM', '/home/candd/www/opencart/upload/system/');
+define('DIR_LANGUAGE', '/home/candd/www/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', '/home/candd/www/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', '/home/candd/www/opencart/upload/system/config/');
+define('DIR_IMAGE', '/home/candd/www/opencart/upload/image/');
+define('DIR_CACHE', '/home/candd/www/opencart/upload/system/cache/');
+define('DIR_DOWNLOAD', '/home/candd/www/opencart/upload/system/download/');
+define('DIR_UPLOAD', '/home/candd/www/opencart/upload/system/upload/');
+define('DIR_LOGS', '/home/candd/www/opencart/upload/system/logs/');
+define('DIR_MODIFICATION', '/home/candd/www/opencart/upload/system/modification/');
+define('DIR_CATALOG', '/home/candd/www/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'candd');
+define('DB_PASSWORD', '1');
+define('DB_DATABASE', 'mbShop');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.116.131/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.116.131/');
+
+// DIR
+define('DIR_APPLICATION', '/home/candd/www/opencart/upload/catalog/');
+define('DIR_SYSTEM', '/home/candd/www/opencart/upload/system/');
+define('DIR_LANGUAGE', '/home/candd/www/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', '/home/candd/www/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/home/candd/www/opencart/upload/system/config/');
+define('DIR_IMAGE', '/home/candd/www/opencart/upload/image/');
+define('DIR_CACHE', '/home/candd/www/opencart/upload/system/cache/');
+define('DIR_DOWNLOAD', '/home/candd/www/opencart/upload/system/download/');
+define('DIR_UPLOAD', '/home/candd/www/opencart/upload/system/upload/');
+define('DIR_MODIFICATION', '/home/candd/www/opencart/upload/system/modification/');
+define('DIR_LOGS', '/home/candd/www/opencart/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'candd');
+define('DB_PASSWORD', '1');
+define('DB_DATABASE', 'mbShop');
+define('DB_PREFIX', 'oc_');
